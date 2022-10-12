b990378b6cdc8c81f475d474cbcdf6bba0c6c34a
Vishnu9535 <vk9255888@gmail.com>
2022-10-12 23:03:53 +0530

    1

--- algorithams/linear.py
+++ algorithams/linear.py
@@ -1,3 +1,11 @@
 import numpy as np
-import torch
- 
+import pandas as pd
+import matplotlib.pyplot as plt
+seed=1234
+num_samples=50
+def generate_data():
+    x=np.array(range(num_samples))
+    #print(x)
+    random_noise=np.random.uniform(-10,20,size=num_samples)
+    print(random_noise)
+generate_data()
