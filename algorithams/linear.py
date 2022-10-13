d1ea9ad99aac651250b57d2d6618c3d847199467
Vishnu9535 <vk9255888@gmail.com>
2022-10-13 20:48:09 +0530

    m12

--- algorithams/linear.py
+++ algorithams/linear.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 seed=1234
-num_samples=50
+NUM_SAMPLE=50
 def generate_data():
+    num_samples=NUM_SAMPLE
     x=np.array(range(num_samples))
     #print(x)
     random_noise=np.random.uniform(-10,20,size=num_samples)
-    print(random_noise)   
-    
-generate_data()
+    #print(random_noise)  
+    y=3.5*x+random_noise
+    #print(y) 
+    data=np.vstack((x,y)).T
+    print(data)
+    return data
+data=generate_data()
