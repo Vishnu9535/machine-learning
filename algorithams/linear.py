ffe4ba2122ad8883d4d980ae56b051dc48be751b
Vishnu9535 <vk9255888@gmail.com>
2022-10-16 14:03:44 +0530

    using numpy

--- algorithams/linear.py
+++ algorithams/linear.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
@@ -12,6 +13,41 @@ def generate_data():
     y=3.5*x+random_noise
     #print(y) 
     data=np.vstack((x,y)).T
-    print(data)
+    #print(data)
     return data
 data=generate_data()
+dataframe=pd.DataFrame(data,columns=["x","y"])
+#print(dataframe)
+X=dataframe[["x"]].values
+Y=dataframe[["y"]].values
+dataframe.head()
+#print(dataframe["x"])
+plt.title("generated data")
+plt.scatter(dataframe["x"],dataframe["y"])
+#plt.show()      
+train_size=0.7
+val_size=0.15
+test_size=0.15
+indicis=list(range(NUM_SAMPLE))
+np.random.shuffle(indicis)
+#print(indicis)
+X=X[indicis]
+Y=Y[indicis]
+train_start=0
+train_end=int(0.7*NUM_SAMPLE)
+val_start=train_end
+val_end=int((train_size+val_size)*NUM_SAMPLE)
+#print(val_end)
+test_start=val_end
+x_train=X[train_start:train_end]
+y_train=Y[train_start:train_end]
+x_val=X[val_start:val_end]
+y_val=Y[val_start:val_end]
+x_test=X[test_start:]
+y_test=Y[test_start:]
+
+
+
+
+
+
