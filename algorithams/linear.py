baad92968e13ab7ef26ac19533b7814c1d1081d4
Vishnu9535 <vk9255888@gmail.com>
2023-01-18 17:52:03 +0530

    ..

--- algorithams/linear.py
+++ algorithams/linear.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 seed = 1234
 NUM_SAMPLE = 50
@@ -8,7 +9,7 @@ NUM_SAMPLE = 50
 def generate_data():
     num_samples = NUM_SAMPLE
     x = np.array(range(num_samples))
-    # print(x)
+    print(x)
     random_noise = np.random.uniform(-10, 20, size=num_samples)
     # print(random_noise)
     y = 3.5*x+random_noise
