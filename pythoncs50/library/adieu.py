01f7c5455f9a57974799c4176538bf5fd6e2b252
Vishnu9535 <vk9255888@gmail.com>
2023-07-17 20:21:35 +0530

    ...

--- pythoncs50/library/adieu.py
+++ pythoncs50/library/adieu.py
@@ -1,1 +1,24 @@
 import inflect 
+import sys
+# p=inflect.engine()
+# animal="lion"
+# print("I saw", N, p.plural_noun(animal, N))
+# print("I saw ", p.no(animal))
+# errors=0
+# print(p.plural_adj("has"))
+names=[]
+while True:
+    try:
+        x=str(input("Name: "))
+        names.append(x)
+    except EOFError:
+        break
+x=len(names)-1
+result="Adieu, adieu, to "
+if x==1:
+    result=result+names[x]
+else:
+    for arg in names[:x]:
+        result=result+arg+", "
+    result=result+"and "+names[x]
+print(result)
