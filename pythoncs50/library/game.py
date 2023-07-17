57e997e78ed6642cfae86edd7938910ae424a969
Vishnu9535 <vk9255888@gmail.com>
2023-07-17 20:39:03 +0530

    ...

--- pythoncs50/library/game.py
+++ pythoncs50/library/game.py
@@ -1,4 +1,12 @@
+import random
 while True:
     try:
         num=int(input("Level: "))
-        
+        if(num > 0):
+            ans=random.randint(1,num)
+            try: 
+                guess=int(input("Guess: "))
+                if(guess > ans):
+                    print("Too large")
+                if(guess < ans):
+                    print("Too small")
