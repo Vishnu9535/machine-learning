25c1e552fce2fd8704e26417b59cb080c02f1dc3
Vishnu9535 <vk9255888@gmail.com>
2023-07-18 14:16:14 +0530

    ....

--- pythoncs50/library/game.py
+++ pythoncs50/library/game.py
@@ -3,10 +3,22 @@ while True:
     try:
         num=int(input("Level: "))
         if(num > 0):
-            ans=random.randint(1,num)
-            try: 
-                guess=int(input("Guess: "))
-                if(guess > ans):
-                    print("Too large")
-                if(guess < ans):
-                    print("Too small")
+            break
+        else:
+             raise Exception
+    except ValueError:
+        pass
+ans=int(random.randint(1,num))
+while True:  
+        try: 
+            guess=int(input("Guess: "))
+            if(guess > ans):
+                print("Too large")
+                raise Exception 
+            elif(guess < ans):
+                print("Too small")
+            elif(guess == ans):
+                 print("Just right")
+                 break
+        except Exception as e:
+                pass
---
+++ pythoncs50/library/professor.py
@@ -0,0 +1,23 @@
+import random
+
+
+def main():
+    x=get_level()
+    generate_integer(x)
+
+def get_level():
+    while True:
+        try:
+            level=int(input("Level: "))
+            if(level==1 or level == 2 or level == 3):
+                return level
+            raise Exception
+        except Exception:
+            pass
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()
