52637f5d15f6189d07ad20ed832e4a64ee883c1b
Vishnu9535 <vk9255888@gmail.com>
2023-07-20 14:43:24 +0530

    hi

--- pythoncs50/fileIO/test.py
+++ pythoncs50/fileIO/test.py
@@ -1,5 +1,11 @@
-x=input("what is ur name ")
+# x=input("what is ur name ")
 
-file=open("name.txt","a")
-file.write(f" {x} \n")
-file.close()
+# file=open("name.txt","a")
+# file.write(f" {x} \n")
+# file.close()
+with open("name.txt" , "r") as file :
+    # file.write(f"{x} \n") 
+    l= file.readlines()
+
+for line in l:
+    print(line.rstrip())
