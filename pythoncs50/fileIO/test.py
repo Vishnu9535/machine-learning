88f55170924c7fadd850cf87c2da40fb171f4863
Vishnu9535 <vk9255888@gmail.com>
2023-07-20 23:10:52 +0530

    whay

--- pythoncs50/fileIO/test.py
+++ pythoncs50/fileIO/test.py
@@ -3,9 +3,22 @@
 # file=open("name.txt","a")
 # file.write(f" {x} \n")
 # file.close()
-with open("name.txt") as file :
-    # file.write(f"{x} \n") 
-    l= file.readlines()
+# with open("name.txt") as file :
+#     # file.write(f"{x} \n") 
+#     l= file.readlines()
 # print(l)
-for line in sorted(l):
-    print(line.rstrip())
+# for line in sorted(l):
+#     print(line.rstrip())
+students=[]
+with open("students.csv") as file:
+     for line in file:
+          name,house=line.rstrip().split(",")
+          student={}
+          student['name']=name
+          student['house']=house 
+          students.append(student)
+
+
+for stu in students:
+     print(stu)
+     print(f"{stu['name']} is in  {stu['house']}")
