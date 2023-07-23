55ec1d4b610852278a096ce176c7160b3a190b49
Vishnu9535 <vk9255888@gmail.com>
2023-07-23 21:28:17 +0530

    jbf

--- pythoncs50/fileIO/scourgify.py
+++ pythoncs50/fileIO/scourgify.py
@@ -1,12 +1,20 @@
 import csv
 import sys
-with open ("before.csv") as file:
-    reader=csv.DictReader(file)
-    with open("after.csv","w") as file2:
-        fn=["first_name","last_name","house"]
-        writer=csv.writer(file2)
-        writer.writerow(fn)
-        writer=csv.DictWriter(file2,fieldnames=fn)
-        for x in reader:
-            first_name,last_name=x['name'].split(', ')
-            writer.writerow({"first_name":first_name,"last_name":last_name,"house":x['house']})
+if(len(sys.argv) > 3):
+    sys.exit("too many arguments")
+elif(len(sys.argv) < 3):
+    sys.exit("too few arguments")
+
+try:
+    with open (sys.argv[1]) as file:
+        reader=csv.DictReader(file)
+        with open(sys.argv[2],"w") as file2:
+            fn=["first_name","last_name","house"]
+            writer=csv.writer(file2)
+            writer.writerow(fn)
+            writer=csv.DictWriter(file2,fieldnames=fn)
+            for x in reader:
+                first_name,last_name=x['name'].split(', ')
+                writer.writerow({"first_name":first_name,"last_name":last_name,"house":x['house']})
+except FileNotFoundError:
+    sys.exit("could not read "+sys.argv[1])
