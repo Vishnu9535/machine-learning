30e8590d7e7cd13ccccb3094cba9f3a2629a09e1
Vishnu9535 <vk9255888@gmail.com>
2023-07-24 10:18:20 +0530

    ud

--- pythoncs50/fileIO/shirt.py
+++ pythoncs50/fileIO/shirt.py
@@ -1,2 +1,9 @@
 import sys
 from PIL import Image
+if len(sys.argv) !=3:
+    sys.exit("enter a valid number of  agument")
+lis=[".jpg",".jpeg",".png"]
+if any(sys.argv[1].endswith(i) for i in lis):
+    sys.exir("not a valid input")
+if any(sys.argv[2].endswith(i) for i in lis):
+    sys.exir("not a valid input")
