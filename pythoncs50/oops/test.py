4aa399f29a40d5784cf484029119638d30b4c07c
Vishnu9535 <vk9255888@gmail.com>
2023-07-25 22:10:24 +0530

    d

---
+++ pythoncs50/oops/test.py
@@ -0,0 +1,10 @@
+class Student:
+    ...
+
+def main():
+    print()
+def get_student():
+    student =Student()
+    student.name=input("Name: ")
+    student.house=input("House: ")
+    return student
