ef67da67e32550ac9961c085cef3837c9b3e5e2f
Vishnu9535 <vk9255888@gmail.com>
2023-07-27 10:15:10 +0530

    bd

--- pythoncs50/oops/test.py
+++ pythoncs50/oops/test.py
@@ -7,18 +7,28 @@ class Student:
     
     def __str__(self):
         return f"{self.name} is form {self.house}"
-    @property
+    @property #getter 
     def house(self): 
-        return self.house
+        return self._house
     @house.setter
     def house(self,house):
         if not house:
             raise ValueError("not a valid house")
-        self.house = house
+        self._house = house
+    
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self,name):
+        if not name:
+            raise ValueError("not a valid name")
+        self._name=name
 
 def main():
     student=get_student()
-    # student.h="vrpura"
+    # student.house=""
     print(student)
 def get_student():
     # student =Student()
