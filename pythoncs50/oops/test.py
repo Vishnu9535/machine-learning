2c12e810fdcae1ef4ce9cec0ebbb4e539b9d0592
Vishnu9535 <vk9255888@gmail.com>
2023-07-27 10:01:04 +0530

    jd

--- pythoncs50/oops/test.py
+++ pythoncs50/oops/test.py
@@ -1,10 +1,29 @@
 class Student:
-    ...
+    def __init__(self,name,house):
+        if not name:
+            raise ValueError("not a valid name") 
+        self.name  =name
+        self.house= house
+    
+    def __str__(self):
+        return f"{self.name} is form {self.house}"
+    @property
+    def house(self): 
+        return self.house
+    @house.setter
+    def house(self,house):
+        if not house:
+            raise ValueError("not a valid house")
+        self.house = house
 
 def main():
-    print()
+    student=get_student()
+    # student.h="vrpura"
+    print(student)
 def get_student():
-    student =Student()
-    student.name=input("Name: ")
-    student.house=input("House: ")
-    return student
+    # student =Student()
+    name=input("Name: ")
+    house=input("House: ")
+    return Student(name,house)#constucter
+if __name__=="__main__":
+    main()
