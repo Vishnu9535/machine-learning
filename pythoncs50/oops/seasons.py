d120c723fe051c43d2d52dffcbfc4c1bd65325f1
Vishnu9535 <vk9255888@gmail.com>
2023-07-28 01:00:52 +0530

    seasons.py

--- pythoncs50/oops/seasons.py
+++ pythoncs50/oops/seasons.py
@@ -2,7 +2,7 @@ from  datetime import datetime
 from datetime import date, timedelta
 import time
 import inflect
-
+import sys
 class findtime:
     def __init__(self):
         self.ptime=str(date.today())  
@@ -11,14 +11,15 @@ class findtime:
     def find_time(self):
         dob=input('Enter your date of birth ')
         try:
-            m=datetime.fromisoformat(dob)
-            self.dob=dob.split('-')
-            # print(self.dob)
-            finaldate=  date(int(self.ptime[0]),int(self.ptime[1]),int(self.ptime[2]))- date(int(self.dob[0]),int(self.dob[1]),int(self.dob[2]))
-            finaltime=int(finaldate.days)*60*24
-            return finaltime
+            datetime.fromisoformat(dob)
         except Exception:
-            print("invalid input of date ")  
+            sys.exit("not a valid dob")
+         # print(self.dob)
+        self.dob=dob.split('-')
+        finaldate=  date(int(self.ptime[0]),int(self.ptime[1]),int(self.ptime[2]))- date(int(self.dob[0]),int(self.dob[1]),int(self.dob[2]))
+        finaltime=int(finaldate.days)*60*24
+        return finaltime
+         
                   
     def string_of_time(self,finaltime):
         p=inflect.engine()     
