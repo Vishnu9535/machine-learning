8619013d5b09a011cac49307a10f668d0b29dc90
Vishnu9535 <vk9255888@gmail.com>
2023-08-04 17:38:38 +0530

    ..

--- algorithams/logistic.py
+++ algorithams/logistic.py
@@ -10,7 +10,7 @@ from sklearn.metrics import confusion_matrix
 
 titanic_data = pd.read_csv("train.csv")
 y = titanic_data["Survived"]
-cclumns_to_drop = ['Survived','Name','Sex','Age','Ticket','Fare','Cabin','Embarked']
+cclumns_to_drop = ['Survived','Name','Sex','Age','Ticket','Cabin','Embarked']
 x = titanic_data.drop(cclumns_to_drop,axis=1)
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state = 1)
 logmodel = LogisticRegression()
@@ -20,6 +20,7 @@ LogisticRegression(C=1.0, class_weight=None, dual= False,
                    max_iter = 100, multi_class='ovr', n_jobs=1,
                    penalty='12',random_state=None, solver = 'liblinear',
                    tol = 0.0001,verbose=0, warm_start=False)
+# print(X_test)
 predictions = logmodel.predict(X_test)
 # print(predictions)
 classification_report(y_test,predictions)
