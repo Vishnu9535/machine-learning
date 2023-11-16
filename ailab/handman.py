d7baee572ad54cfe050261c70ef9b9e6577bacd2
Vishnu9535 <2110291@reva.edu.in>
2023-11-16 15:34:07 +0530

    jfd

--- ailab/handman.py
+++ ailab/handman.py
@@ -1,5 +1,17 @@
-word = 'Vishnu'
-
+word = "Vishnu"
+print(word.replace)
 def disply():
-    for i in word:
+    for i in range(len(word)):
+        print("_ ", end = " ")
+disply()
+attempts = 6
+def guess():
+    while attempts > 0 :
+        letter = input("guess the letter ")
+        guess_word = ""
+        if letter in word:
+            guess_word.append()
+            word=word.replace(letter,'')
         
+
+
